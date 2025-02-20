dc14a9ea97181f108b3a564377283f46bc46b936
OmarChoi <95075545+OmarChoi@users.noreply.github.com>
2025-02-20 15:55:09 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1620 ms, Memory: 219480 KB -BaekjoonHub

---
+++ C++17/백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+constexpr int MAX = 100'000'000'1;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int nNum, length;
+	cin >> nNum >> length;
+
+	queue<int> numQueue;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, 
+		greater<pair<int, int>>> MinQueue;	// <Value, Index>
+
+	for (int i = 1; i <= length - 1; ++i) {
+		MinQueue.emplace(MAX, -i);
+		numQueue.emplace(MAX);
+	}
+
+	int minValue = MAX;
+	for (int i = 0; i < nNum; ++i) {
+		int newNum = 0;
+		cin >> newNum;
+
+		int OutNum = numQueue.front();
+		numQueue.pop();
+
+		numQueue.emplace(newNum);
+		MinQueue.emplace(newNum, i);
+
+		while (!MinQueue.empty()) {
+			pair<int, int> top = MinQueue.top();
+			if (top.second > i - length && top.second <= i) {
+				break;
+			}
+			MinQueue.pop();
+		}
+
+		if (!MinQueue.empty())
+			cout << MinQueue.top().first << " ";
+	}
+}
